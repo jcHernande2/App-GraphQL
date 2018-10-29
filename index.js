412f0a00e792d44792d70d1a449ddef6046f29d3
jhernandez <jhernandez@MDC-TIC-0796>
2018-10-29 16:58:26 -0600

    removiendo archivos no necesarios

---
+++ index.js
@@ -0,0 +1,50 @@
+const express=require('express')
+const app=express();
+
+const express_graphql=require('express-graphql');
+const {buildSchema} =require('graphql');
+
+//data
+const {courses} =require('./data.json');
+
+const schema=buildSchema(`
+type Query {
+    course(id:Int!):Course
+    courses(topic:String):[Course]
+} 
+
+type Course{
+    id:Int,
+    title:String,
+    author:String,
+    topic:String,
+    url:String
+}
+`);
+let getCourse=(args)=>{
+    let id=args.id;
+    return courses.filter(course=>{
+        return course.id==id;
+    })[0]
+}
+let getCourses=(args)=>{
+    if(args.topic){
+        let topic=args.topic;
+        return courses.filter(course=>course.topic===topic)
+    }else{
+        return courses;
+    }
+}
+const root ={
+    course:getCourse,
+    courses:getCourses
+   // message:()=>"Hello word!"
+}
+
+app.use('/graphql',express_graphql({
+    schema:schema,
+    rootValue:root,
+    graphiql:true
+}));
+
+app.listen(3000,()=>console.log("server puerto 3000"));
